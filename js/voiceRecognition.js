09750c4395bed8e2cd45f4cb598462a14f38cf00
matheus-merllo <matheusmerllo97@gmail.com>
2023-02-13 12:14:37 -0300

    reconhecimento de voz incluído

---
+++ js/voiceRecognition.js
@@ -0,0 +1,22 @@
+const elementAttempt = document.getElementById('attempt');
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+recognition.start();
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(event){
+    const attempt = (event.results[0][0].transcript);
+    showAttempt(attempt);
+}
+
+function showAttempt(attempt){
+    elementAttempt.innerHTML = 
+        `<p class="text">Você disse:</p>
+        <span class="box">${attempt}</span>`
+}
+
+recognition.addEventListener('end', () => {
+    recognition.start();
+})
